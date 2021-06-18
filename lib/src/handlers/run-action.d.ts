27205877313db3285267804c7f704f70c0e398a5
Thomas Milotti <thomas.milotti@gmail.com>
2021-06-18 19:08:00 +0200

    rebuild

--- lib/src/handlers/run-action.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context, WebhookPayloadWithRepository } from 'probot';
-import { AppConfig } from '../config';
-export declare const runOrgAction: (appConfig: AppConfig) => (context: Context<WebhookPayloadWithRepository>) => Promise<void>;
